2ecd8ccddfc265eab98ad82886696b8bd884c376
mengfeijin <jin.xzy@outlook.com>
2026-03-08 04:04:02 -0700

    update deploy.yml

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World! \n Test Test!'
+    return 'Hello World! \n Test Test! 吊不吊吊不吊!!!!说话！！！！'
 
 
 if __name__ == '__main__':

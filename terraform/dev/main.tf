56349814823e263ebb79036b67b13235f3ed29c4
mengfeijin <jin.xzy@outlook.com>
2026-06-11 18:13:55 -0700

    Upload image_0

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -17,13 +17,68 @@ resource "aws_instance" "worker_node" {
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.practice_sg.id]
 
+  iam_instance_profile   = aws_iam_instance_profile.ec2_s3_profile.name
+
   tags = {
     Name = "${var.project_name}-Worker"
-  }
-}
+  }}
 
 #Generate server groups in ansible .ini.
 resource "local_file" "ansible_inventory" {
   content  = "[web_servers]\n${aws_instance.worker_node.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=../keys/practice.pem"
   filename = "../../ansible/inventory.ini"
+}
+
+resource "aws_s3_bucket" "app_uploads" {
+  bucket = "peter-app-uploads-bucket-7689"
+}
+
+resource "aws_iam_role" "ec2_s3_access_role" {
+  name = "ec2_s3_access_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_upload_policy" {
+  name        = "s3_upload_policy"
+  description = "Allows EC2 to upload objects to the app uploads S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          aws_s3_bucket.app_uploads.arn,
+          "${aws_s3_bucket.app_uploads.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "s3_access_attach" {
+  role       = aws_iam_role.ec2_s3_access_role.name
+  policy_arn = aws_iam_policy.s3_upload_policy.arn
+}
+
+resource "aws_iam_instance_profile" "ec2_s3_profile" {
+  name = "ec2_s3_profile"
+  role = aws_iam_role.ec2_s3_access_role.name
 }
